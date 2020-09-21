5d127f3dbad7a6234b3945d9106be4f5a65e81ce
Ruchi Kushwaha <65845928+Ruchi27280@users.noreply.github.com>
2020-09-21 19:25:56 +0530

    Update pyfile_1.py

--- pyfile_1.py
+++ pyfile_1.py
@@ -1,13 +1,3 @@
-#base,height = map(int,input("enter your base and height").split())
-#print(base)
-#print(height)
-#area = (base*height)/2
-#print("area of triangle" + str(area))
-# def addtwo(a, b):
-#     added = a + b
-#     return a
-# x = addtwo(2, 7)
-# print(x)
 list = ['ruchi','rakhi','alok','akash']
 def func(list,**kwargs):
     # return [i[:].title() for i in list]
