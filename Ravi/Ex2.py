ba095295722de182725f63ebfe81a681fa2f7e71
Ravik27280 <Ravik27280@gmaill.com>
2020-09-22 21:01:03 +0530

    update

--- Ravi/Ex2.py
+++ Ravi/Ex2.py
@@ -1,1 +1,8 @@
 #Given a string, display only those characters which are present at an even index number.
+a= input("enter string = ")
+l= len(a)
+print(l)
+for i in range(0,l):
+    if i%2==0:
+        print(a[i])
+    
