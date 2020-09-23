107e45392858a861ab496054c8ca30b02744a00b
Ravik27280 <Ravik27280@gmaill.com>
2020-09-23 19:59:52 +0530

    ex3 ravi

--- Ravi/Ex3.py
+++ Ravi/Ex3.py
@@ -2,3 +2,9 @@
 # return a new string
 # For example, removeChars("pynative", 4) so 
 # output must be tive. Note: n must be less than the length of the string.
+def removechar():
+    a=input("enter a string= ")
+    n=int(input("enter value of n= "))
+    if n<len(a):
+        print(a[n:])
+removechar()
