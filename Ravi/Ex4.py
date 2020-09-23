05499a3db96bb92e5b47b84c42b1767f28834507
Ravik27280 <Ravik27280@gmaill.com>
2020-09-23 21:11:34 +0530

    solved

--- Ravi/Ex4.py
+++ Ravi/Ex4.py
@@ -4,7 +4,7 @@
 # 3 3 3 
 # 4 4 4 4
 # 5 5 5 5 5
-for i in range(1,4):#i=2
+for i in range(1,6):#i=2
     for j in range(i):#j=1
         print(i,end=" ")
     print()
--- Ravi/Ex5.py
+++ Ravi/Ex5.py
@@ -1,1 +1,7 @@
 # Reverse a given number and return true if it is the same as the original number
+n= input("enter a no. = ")
+r=n[::-1]
+if n==r:
+    print("true")
+else:
+    print("false")
