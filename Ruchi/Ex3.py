b072ae0e04950104b8c46fe8da3eab1914fefe03
Ruchi Kushwaha <65845928+Ruchi27280@users.noreply.github.com>
2020-09-23 20:01:18 +0530

    update exercise3

--- Ruchi/Ex3.py
+++ Ruchi/Ex3.py
@@ -2,3 +2,16 @@
 # return a new string
 # For example, removeChars("pynative", 4) so 
 # output must be tive. Note: n must be less than the length of the string.
+string = input("enter any string")
+num = int(input("enter a number"))
+if num < len(string):
+    print(string[num:])
+else:
+    print("give no. which is less than length of string")
+# def func(string,num):
+#     if num < len(string):
+#         return string[num:]
+#     else:
+#         return "wrong input"
+# print(func(string,num))
+
