1a28fc93d88b507951281f29c055e72a8e3e785d
Ruchi Kushwaha <65845928+Ruchi27280@users.noreply.github.com>
2020-09-24 20:31:35 +0530

    update

---
+++ Ravi/Ex6.py
@@ -0,0 +1,11 @@
+# l = []
+
+# for i in range(4):
+#     l.append(input("enter"))
+# print(l)
+
+a = [1,5,6,1,3,5,7,2,1,0,4,1,1]
+for i in a:
+    if i<= 5:
+        print(i,end=",")
+        print(list(i))
