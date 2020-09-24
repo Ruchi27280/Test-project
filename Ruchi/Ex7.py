8003bede45e2da5df32e2577388c996527a64c07
Ravik27280 <Ravik27280@gmaill.com>
2020-09-24 20:13:04 +0530

    parter qustions

---
+++ Ruchi/Ex7.py
@@ -0,0 +1,78 @@
+# # # 1  
+
+# # # 2 2  
+
+# # # 3 3 3  
+
+# # # 4 4 4 4  
+
+# # # 5 5 5 5 5
+
+# # for i in range(1,6):
+# #     for j in range(i):
+# #         print (i,end=" ")
+# #     print("\n")
+
+# # 1 1 1 1 1 
+
+# # 2 2 2 2 
+
+# # 3 3 3 
+
+# # 4 4 
+
+# # 5
+
+# for i in range(1,6):
+#     for j in range(i,6):
+#         print(i,end=" ")
+#     print()
+
+
+# 1 
+
+# 1 2 
+
+# 1 2 3 
+
+# 1 2 3 4 
+
+# 1 2 3 4 5
+
+# for i in range(1,6):
+#     for j in range (1,i+1):
+#         print(j,end=" ")
+#     print()
+
+# 5 5 5 5 5 
+
+# 4 4 4 4 
+
+# 3 3 3 
+
+# 2 2 
+
+# 1
+
+# for i in range(1,6):
+#     for j in range(i,6):
+#         print(6-i, end=" ")
+#     print()
+    
+# 5 5 5 5 5 
+
+# 5 5 5 5 
+
+# 5 5 5 
+
+# 5 5 
+
+# 5
+
+for i in range(1,6):
+    for j in range(i,6):
+        print(5,end=" ")
+    print("\n")
+
+
+
